0c08957b8ee69e0e0d6c8ebe0e229244c679a8b4
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-02-16 23:20:37 +0300

    I'm gonna change it in absolutely new way - synchronization!

---
+++ Networking/MyLibrary/include/connection.h
@@ -0,0 +1,25 @@
+#ifndef TEAMPROJECT_CONNECTION_H
+#define TEAMPROJECT_CONNECTION_H
+#include <string>
+#include <iostream>
+#include <memory>
+#include <boost/asio.hpp>
+namespace myServer{
+    using boost::asio::ip::tcp;
+    struct TCPConnection : std::enable_shared_from_this<TCPConnection> {
+        using ptr_t = std::shared_ptr<TCPConnection>;
+
+        static ptr_t create_ptr(boost::asio::io_context &io_context);
+
+        tcp::socket& socket();
+
+        void Start();
+    private:
+        explicit TCPConnection(boost::asio::io_context &io_context);
+    private:
+        tcp::socket _socket;
+        std::string _msg{"Hi, Client!"};
+    };
+}
+
+#endif //TEAMPROJECT_CONNECTION_H
