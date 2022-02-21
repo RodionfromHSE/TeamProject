ba5c16b522793d0ceed15b44b6a3661665beb98e
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-02-21 23:02:55 +0300

    Fixed!

---
+++ Networking/MyLibrary/cmake-build-debug/CMakeFiles/MyLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyLibrary.dir/connection.cpp.o"
+  "CMakeFiles/MyLibrary.dir/main.cpp.o"
+  "CMakeFiles/MyLibrary.dir/server.cpp.o"
+  "MyLibrary"
+  "MyLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Networking/build/MyClient/CMakeFiles/MyClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyClient.dir/main.cpp.o"
+  "MyClient"
+  "MyClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
