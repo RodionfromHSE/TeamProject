c5160327301c62e7913bceed73235c71ae65d931
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-03-28 23:16:14 +0300

    Server headers

---
+++ Networking/cmake-build-debug/Client/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../Client/Client.exe"
+  "../../Client/Client.exe.manifest"
+  "../../Client/Client.pdb"
+  "CMakeFiles/Client.dir/client.cpp.obj"
+  "CMakeFiles/Client.dir/main.cpp.obj"
+  "libClient.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Networking/cmake-build-debug/Server/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../Server/Server.exe"
+  "../../Server/Server.exe.manifest"
+  "../../Server/Server.pdb"
+  "CMakeFiles/Server.dir/main.cpp.obj"
+  "CMakeFiles/Server.dir/server.cpp.obj"
+  "libServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
