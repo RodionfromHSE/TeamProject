398b176c16b25daec6dc3a085ec0de7d73b46bbb
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-04-18 23:43:54 +0300

    NetworkSystem description

--- Game/NetworkSystem.h
+++ Game/NetworkSystem.h
@@ -2,8 +2,22 @@
 
 #include "fwd.h"
 #include "system.h"
+#include "../Networking/Client/client.h"
+#include "../Networking/Library/synchronizied.h"
+
+struct PlayerType;
 
 struct NetworkSystem : System {
+    // TODO: Synchronize Player
+    NetworkSystem(PlayerType& Player);
+    // TODO: For client - not our case
+    bool connect(std::string &host);
+    // Hmm... update something
+    void update() override;
 
+    void make_synch
+private:
+    std::shared_ptr<Client> m_clientPtr;
+    std::vector<net::Synchronized<PlayerType>> m_players;
 };
 
