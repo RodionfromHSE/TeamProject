390d6528ba1a3b0a4ec44a6a73cc21af8a3e4be8
Aleksandr <amosin2002@mail.ru>
2022-05-26 22:09:01 +0300

    network fix

--- Game/network_system.cpp
+++ Game/network_system.cpp
@@ -1,26 +1,22 @@
 #include "network_system.h"
 
-NetworkSystem::NetworkSystem(uint16_t port) : m_serverPtr(new Server(port)) {
-    thread = std::thread([this](){
-        while (true){
+ClientSystem::ClientSystem(uint16_t port) : m_clientPtr(new Client()), m_player(m_clientPtr, PLAYER_ID) {
+    m_clientPtr->connect(host, PORT);
+    m_player.set({0, 0});
+
+    thread = std::thread([this]() {
+        while (true) {
             update();
         }
     });
 }
 
-void NetworkSystem::update() {
+void ClientSystem::update() {
     // Here we'll new players arrive and old update
-    m_serverPtr->update();
+    m_clientPtr->update();
 
-    // Arriving newbies
-    auto newbies = m_serverPtr->syn_handler().get_newbies();
-    for (auto nb : newbies){
-        m_players.emplace_back(m_serverPtr, nb);
-    }
+    if (m_player.isUpdatable())
+        std::cout << m_player.get().x << ' ' << m_player.get().y << '\n';
 
-    for (auto &player : m_players) {
-        if (player.isUpdatable())
-            std::cout << player.get().x << ' ' << player.get().y << '\n';
-    }
 }
 
--- Game/network_system.h
+++ Game/network_system.h
@@ -5,26 +5,33 @@
 #include "../Networking/Client/client.h"
 #include "../Networking/Server/server.h"
 #include "../Networking/Library/synchronizied.h"
+#include "../Networking/Library/synchronizied_handler.h"
 
 struct PlayerCoordinates {
     int x = 0, y = 0;
 };
 
+const std::string host = "127.0.0.1";
+const int PORT = 60'000;
+const int PLAYER_ID = 770;
 
-struct NetworkSystem : System {
+struct ClientSystem : System {
     // TODO: Synchronize Player
-    NetworkSystem(uint16_t port);
+    ClientSystem(uint16_t port);
 
     // Hmm... update something
     void update() override;
 
-    inline ~NetworkSystem(){
+    PlayerCoordinates getPlayer(){ return m_player.get(); }
+    void setCoors(int x, int y){ m_player.set({x, y}); }
+
+    inline ~ClientSystem(){
         if (thread.joinable()) { thread.join(); }
     }
 
 private:
-    std::shared_ptr<Server> m_serverPtr;
-    std::vector<net::Synchronized<PlayerCoordinates>> m_players;
+    std::shared_ptr<Client> m_clientPtr;
+    net::Synchronized<PlayerCoordinates, Parameter::OnClient> m_player;
     std::thread thread;
 };
 
